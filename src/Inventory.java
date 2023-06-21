e885a16e3a302e2ff6e95f1087c540fc9e77c36b
working05 <hty050216@gmail.com>
2023-06-21 12:18:29 +0900

    Inventory, Item
    Inventory, Item 진짜 기본 틀만 잡음

---
+++ src/Inventory.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class Inventory {
+    ArrayList<Item> itemList;
+    boolean isShowing;
+
+    public Inventory() {
+        itemList = new ArrayList<>();
+        isShowing = false;
+    }
+
+    void display() {
+        
+    }
+
+    void pushItem(Item item) {
+        itemList.add(item);
+    }
+
+    Item popItem(int i) {
+        Item item = itemList.get(i);
+        itemList.remove(i);
+
+        return item;
+    }
+
+    void openInventory() {
+
+    }
+}
---
+++ src/Item.java
@@ -0,0 +1,9 @@
+public class Item {
+    String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+
+}

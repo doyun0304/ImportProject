183ddcd6510fdef3f463e99d8b2d465779aafde2
working05 <hty050216@gmail.com>
2023-07-16 02:08:32 +0900

    부드럽게 걷기
    사람의 움직임을 더블로 처리하여 조금더 작은 간격으로 움직이게 했소이다.

--- src/render/MainPanel.java
+++ src/render/MainPanel.java
@@ -5,4 +5,5 @@ import javax.swing.*;
 // Panel for Main screen - stage selection or start screen etc.
 
 public class MainPanel extends JPanel {
+  
 }
--- src/world/entity/character/Player.java
+++ src/world/entity/character/Player.java
@@ -2,6 +2,7 @@ package world.entity.character;
 
 import render.GamePanel;
 import render.Images;
+import util.DVec2D;
 import util.Vec2D;
 
 import java.awt.*;
@@ -10,15 +11,17 @@ import java.awt.image.BufferedImage;
 import static render.RenderUtil.tileSize;
 
 public class Player {
-    private Vec2D position;
+    private DVec2D position;
     Direction direction;
-    private int velocity = 1;
+
+    public int n = 4;
+    private double velocity = 0.25;
     private int moveCondition;
     GamePanel gamePanel;
     BufferedImage[][] img;
 
     public Player(){
-        position = new Vec2D();
+        position = new DVec2D();
         direction = Direction.UP;
         img = Images.playerImage;
     }
@@ -36,32 +39,30 @@ public class Player {
         if(canMove(direction)) {
             switch (direction) {
                 case UP:
-                    position.add(new Vec2D(0, -velocity));
+                    position.add(new DVec2D(0, -velocity));
                     break;
                 case LEFT:
-                    position.add(new Vec2D(-velocity, 0));
+                    position.add(new DVec2D(-velocity, 0));
                     break;
                 case DOWN:
-                    position.add(new Vec2D(0, velocity));
+                    position.add(new DVec2D(0, velocity));
                     break;
                 case RIGHT:
-                    position.add(new Vec2D(velocity, 0));
+                    position.add(new DVec2D(velocity, 0));
                     break;
             }
         }
     }
 
     public void draw(Graphics2D g2){
-        g2.drawImage(img[direction.ordinal()][moveCondition], position.x*tileSize, position.y*tileSize, null);
+        g2.drawImage(img[direction.ordinal()][moveCondition], (int)(position.x*tileSize), (int)(position.y*tileSize), null);
     }
 
     private boolean canMove(Direction direction){
         return true;
     }
 
-    public Vec2D getPosition() {
-        return position;
-    }
+    public Vec2D getPosition() {return new Vec2D((int)position.x, (int)position.y);}
 
     public void updateMoveCondition(boolean stop) {
         if(stop) moveCondition = 0;

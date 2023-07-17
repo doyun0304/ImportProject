73c5d64e28e85b8797cbe73e5e638b4e25104a51
doyun0304 <ddyy0304@gmail.com>
2023-07-18 00:08:30 +0900

    Update Frame size

--- src/main/ImportProject.java
+++ src/main/ImportProject.java
@@ -5,8 +5,8 @@ import java.awt.*;
 
 public class ImportProject {
     private static JFrame screen;
-    public static final int screenWidth = 1080;
-    public static final int screenHeight = 800;
+    public static final int screenWidth = 1000;
+    public static final int screenHeight = 680;
     public static GameManager gameManager;
 
     public static void main(String[] args) {
@@ -19,11 +19,10 @@ public class ImportProject {
         screen.setTitle("Room Escape");
         screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         screen.setResizable(false);
-        screen.setSize(screenWidth, screenHeight);
-        screen.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        screen.getContentPane().setPreferredSize(new Dimension(screenWidth, screenHeight));
         screen.getContentPane().setBackground(Color.BLACK);
         gameManager.initiate(screen);
-
         screen.setVisible(true);
+        screen.pack();
     }
 }
